4d1c9015d30d34740dfb28d09822d5a8df49e842
Jared Bautista <jdb@mbp.attlocal.net>
2022-03-04 14:35:51 -0800

    first commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+
+import discord
+import random
+
+TOKEN = 'OTQ5NDE1NjQ3Mzc1NDA1MTE3.YiKCOQ.gt2pRHRJdlwlw5HgKqK1EY-F8ts'
+
+pics = [f for f in os.listdir('.') if os.path.isfile(f) and f[-3:] == 'JPG']
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if ('!coach' in message.content):
+        if pics:
+            pic = random.choice(pics)
+            await message.channel.send(file=discord.File(pic))
+        else:
+            await message.channel.send('I can\'t find any pics :(')
+
+client.run(TOKEN)
